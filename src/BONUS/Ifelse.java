1dbfc9de943ff7eead53c9542791672e7fb9b651
AnneKluytmans <annekluytmans1@gmail.com>
2024-09-07 12:01:43 +0200

    Updated bonus assignment if-else

--- src/BONUS/Ifelse.java
+++ src/BONUS/Ifelse.java
@@ -2,23 +2,83 @@ package BONUS;
 
 public class Ifelse {
     public static void main(String[] args) {
-        int input1 = 3;
-        double grade = 6.3;
-        int age = 53;
-        String text = "Is this a long text?";
-        int year = 2024;
+
+        // Call evenOrOdd
+        int input1 = 8;
+        int input2 = 1;
+        int input3 = -1;
+        int input4 = 0;
 
         evenOrOdd(input1);
-        numberToGrade(grade);
-        biggestNumber(8, 4);
-        positiveOrZeroOrNegative(-9);
-        ageCheck(age);
-        textLength(text);
-        leapYear(year);
-        biggestNumber(2, 6, 9);
-        triangleType(3,3,6);
-        calculator(2.5, 5.7, '*');
+        evenOrOdd(input2);
+        evenOrOdd(input3);
+        evenOrOdd(input4);
 
+        // Call numberToGrade
+        double gradeFrance = 6.3;
+        double gradeEnglish = 7.5;
+        double gradeMath = 3.6;
+
+        numberToGrade(gradeFrance);
+        numberToGrade(gradeEnglish);
+        numberToGrade(gradeMath);
+        numberToGrade(-3.0);
+
+        //Call sortNumbers and biggest
+        sortNumbers(8, 4);
+        sortNumbers(0, 3);
+        sortNumbers(2, -2);
+
+        sortNumbers(2, 6, 9);
+        sortNumbers(3, -3, -2);
+        sortNumbers(3, 6, 9);
+
+        biggestNumber(3, 8, 6);
+        biggestNumber(5, 2, -3);
+        biggestNumber(5, -6, 9);
+
+        //Call positiveOrZeroOrNegative
+        positiveOrZeroOrNegative(-995);
+        positiveOrZeroOrNegative(305);
+        positiveOrZeroOrNegative(0);
+        positiveOrZeroOrNegative(1);
+        positiveOrZeroOrNegative(-1);
+
+        //Call ageCheck
+        int[] ages = {1, 9, 10, 11, 17, 18, 19, 30, 64, 65, 66, 100};
+        for (int i : ages){
+            ageCheck(i);
+        }
+
+        //Call textLength
+        textLength("Is this a long text?");
+        textLength("Short");
+        textLength("Half long text");
+
+        //Call leapYear
+        int year1 = 2024;
+        int year2 = 1655;
+        int year3 = 2000;
+        int year4 = 1900;
+
+        leapYear(year1);
+        leapYear(year2);
+        leapYear(year3);
+        leapYear(year4);
+
+        //Call triangleType
+        triangleType(4, 4, 4);
+        triangleType(3,3,6);
+        triangleType(2,8,2);
+        triangleType(1, 2, 4);
+
+        //Call calculator
+        calculator(2.5, 5.7, '*');
+        calculator(3, 7.2, '+');
+        calculator(1, -2, '-');
+        calculator(7.6, 6, '/');
+        calculator(1.5, 0, '/');
+        calculator(6.4, 2.5, 'x');
     }
 
     //Assignment 1
@@ -33,22 +93,22 @@ public class Ifelse {
     //Assignment 2
     public static void numberToGrade(double grade) {
         if (grade > 0 && grade <= 5.5) {
-            System.out.println("The grade " + grade + " is the same as grade F");
-        } else if (grade < 6.0) {
-            System.out.println("The grade " + grade + " is the same as grade D");
-        } else if (grade < 7.0) {
-            System.out.println("The grade " + grade + " is the same as grade C");
-        } else if (grade < 8.5) {
-            System.out.println("The grade " + grade + " is the same as grade B");
-        } else if (grade <= 10.0) {
-            System.out.println("The grade " + grade + " is the same as grade A");
+            System.out.println("Grade " + grade + " is the same as grade F");
+        } else if (grade > 0 && grade < 6.0) {
+            System.out.println("Grade " + grade + " is the same as grade D");
+        } else if (grade > 0 &&  grade < 7.0) {
+            System.out.println("Grade " + grade + " is the same as grade C");
+        } else if (grade > 0 &&  grade < 8.5) {
+            System.out.println("Grade " + grade + " is the same as grade B");
+        } else if (grade > 0 &&  grade <= 10.0) {
+            System.out.println("Grade " + grade + " is the same as grade A");
         } else {
             System.out.println("Not a valid grade");
         }
     }
 
     //Assignment 3
-    public static void biggestNumber(int number1, int number2) {
+    public static void sortNumbers(int number1, int number2) {
         if (number1 > number2) {
             System.out.println(number1 + " > " + number2);
         } else {
@@ -59,24 +119,24 @@ public class Ifelse {
     //Assignment 4
     public static void positiveOrZeroOrNegative(int number) {
         if (number > 0) {
-            System.out.println("This number is positive!");
+            System.out.println(number + " is a positive number!");
         } else if (number == 0) {
-            System.out.println("This number is zero!");
+            System.out.println(number + " is zero!");
         } else {
-            System.out.println("This number is negative!");
+            System.out.println(number + " is a negative number!");
         }
     }
 
     //Assignment 5
     public static void ageCheck(int age) {
         if (age < 10) {
-            System.out.println("This person is a child.");
+            System.out.println("A person with age " + age + " is a child.");
         } else if (age < 18) {
-            System.out.println("This person is an adolescent.");
+            System.out.println("A person with age " + age + " is an adolescent.");
         } else if (age < 65) {
-            System.out.println("This person is an adult.");
+            System.out.println("A person with age " + age + " is an adult.");
         } else {
-            System.out.println("This person is a senior.");
+            System.out.println("A person with age " + age + " is a senior.");
         }
     }
 
@@ -104,7 +164,7 @@ public class Ifelse {
 
 
     //Assignment 8
-    public static void biggestNumber (int number1, int number2, int number3) {
+    public static void sortNumbers (int number1, int number2, int number3) {
         if (number1 >= number2 && number1 >= number3) {
             if (number2 >= number3) {
                 System.out.println(number1 + " > " + number2 + " > " + number3);
@@ -126,14 +186,29 @@ public class Ifelse {
         }
     }
 
+    //Assingment 8 *other solution*
+    public static void biggestNumber (int number1, int number2, int number3) {
+        int biggest = number1;
+
+        if (number2 > biggest) {
+            biggest = number2;
+        }
+
+        if (number3 > biggest) {
+            biggest = number3;
+        }
+
+        System.out.println("The biggest number of " + number1 + ", " + number2 + " and " + number3 + " is " + biggest);
+    }
+
     //Assignment 9
     public static void triangleType(int a, int b, int c) {
         if (a == b && b == c) {
-            System.out.println("The triangle is equilateral");
+            System.out.println("The triangle with sides " + a + ", " + b + " and " + c + " is equilateral");
         } else if (a == b || b == c || a == c) {
-            System.out.println("The triangle is isosceles");
+            System.out.println("The triangle with sides " + a + ", " + b + " and " + c + " is isosceles");
         } else {
-            System.out.println("The triangle is unilateral");
+            System.out.println("The triangle with sides " + a + ", " + b + " and " + c + " is unilateral");
         }
     }
 
